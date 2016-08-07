a89f2b02337c1ab9bcea1b5bc7a13e0ad80a6217
Jan Milik <jan.milik@gmail.com>
2016-08-07 06:45:35 +0000

    Fix few problems with urls and templates.

--- django_homis/urls.py
+++ django_homis/urls.py
@@ -9,18 +9,20 @@ import homis_core.views
 import finances.views
 
 
+ROOT_URL = r"^homis/"
+
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'django_homis.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls), name = "admin"),
+    url(ROOT_URL + r'$',         finances.views.index),
 
-    #url(r'^$', "finances.views.index"),
-    url(r'^$', finances.views.index),
+    url(ROOT_URL + r'finances/', include('finances.urls', namespace = "finances")),
+    url(ROOT_URL + r'core/',     include('homis_core.urls', namespace = "homis_core")),
 
-    url(r'^finances/', include('finances.urls', namespace = "finances")),
-    url(r'^core/', include('homis_core.urls', namespace = "homis_core")),
+    url(ROOT_URL + r'admin/',    include(admin.site.urls), name = "admin"),
 ]
 
 
